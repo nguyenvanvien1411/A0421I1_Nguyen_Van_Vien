a8f7dd4f3b44481061b79fafec2f0f1b7413f2a5
Nguyễn Văn Viễn <nguyenvanvien@MacBook-Pro-cua-Nguyen.local>
2021-05-17 12:16:12 +0700

    updatebaitap

---
+++ moduel 1/Javascript/Thuc_hanh/Hien_Thi_dau_ra/main.js
@@ -0,0 +1,15 @@
+function showMessage1() {
+    alert("Hello!!!");
+}
+
+function showMessage2() {
+    document.getElementById('messageDiv').innerHTML = 'Xin Chao!';
+}
+
+function showMessage3() {
+    document.write('Hi!');
+}
+
+function showMessage4() {
+    console.log('Chao ban!!!'); // Co the xem duoc ket qua tren mang hinh console
+}
