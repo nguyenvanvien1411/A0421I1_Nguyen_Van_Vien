250750a770f8547fc21a9ccba3cf4a9669ba4f91
Nguyễn Văn Viễn <nguyenvanvien@MacBook-Pro-cua-Nguyen.local>
2021-05-20 18:55:31 +0700

    updatebaitap

---
+++ moduel 1/Javascript/Thuc_hanh/[Thực Hành] Sử dụng toán tử/js.js
@@ -0,0 +1,6 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let area = inputWidth * inputHeight;
+document.write("The area is: " + area);
